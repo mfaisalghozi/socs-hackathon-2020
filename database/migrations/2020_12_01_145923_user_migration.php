e5cd2d439fb6720dcaba823b7b3512c6d627a9dd
NikolausYN <nikolaus.yesua@gmail.com>
2020-12-01 23:40:22 +0700

    buat db

---
+++ database/migrations/2020_12_01_145923_user_migration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('User', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->string('password', 50);
+            $table->bigInteger('phone');
+            $table->integer('roleid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('User');
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        
+        DB::table('user')->insert([
+            'name' => 'Admin niBos',
+            'email' => 'adminnibos@gmail.com',
+            'password' => 'admin',
+            'phone' => 12345678910,
+            'roleid' => 3,
+       
+        ]);
+
+    	for($i = 1; $i <= 10; $i++){
+            DB::table('user')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password,
+                'phone' => $faker->numberBetween($min = 621111111111, $max=629999999999),
+                'roleid' => $faker->numberBetween($min = 1, $max=2),
+            ]);
+        }
+    }
+}
