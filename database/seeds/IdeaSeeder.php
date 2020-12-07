7a4ccab373fb12ca0e2bfa19121cbff64cac7490
mfaisalghozi <mrafgaming@gmail.com>
2020-12-07 18:47:31 +0700

    fixing db

--- database/seeds/IdeaSeeder.php
+++ database/seeds/IdeaSeeder.php
@@ -13,11 +13,25 @@ class IdeaSeeder extends Seeder
     public function run()
     {   
         // HTR T_T
-        DB::table('Idea')->insert([
-            ['userid' => 2, 'categoryid' => 3, 'ideaname' => 'Pintu Kemana Saja', 'ideaimg' => 'https://picsum.photos/288/163', 'ideadescription'=>'Inilah pintu ajaib yang kalau dibuka bisa membawamu kemana saja.', 'ideatarget'=>'10000000', 'currentearning'=>'100000', 'donatorcount'=>'2','tierAmount'=>'1', 'ideadeadline'=>'2020-12-12', 'qa'=>'Apakah bisa dipakai untuk pergi ke kamar Shizuka? Tentu saja bisa hehe.'],
-            ['userid' => 3, 'categoryid' => 3, 'ideaname' => 'Baling-Baling Bambu', 'ideaimg' => 'https://picsum.photos/288/163', 'ideadescription'=>'Inilah baling-baling bambu yang bisa membuatmu terbang dengan mudah.', 'ideatarget'=>'25000000', 'currentearning'=>'10000000', 'donatorcount'=>'20','tierAmount'=>'1', 'ideadeadline'=>'2020-12-25', 'qa'=>'Apakah bisa dipakai untuk terbang ke hati Shizuka? Tentu saja bisa hehe.'],
-            ['userid' => 4, 'categoryid' => 3, 'ideaname' => 'Baling-Baling Baja', 'ideaimg' => 'https://picsum.photos/288/163', 'ideadescription'=>'Inilah baling-baling baja yang bisa memberikan beban dikepalamu dengan mudah.', 'ideatarget'=>'66600000', 'currentearning'=>'10000000', 'donatorcount'=>'666','tierAmount'=>'1', 'ideadeadline'=>'2020-12-25', 'qa'=>'Apakah bisa dipakai untuk memukul pak guru? Tentu saja bisa hehe.'],
-            ['userid' => 5, 'categoryid' => 3, 'ideaname' => 'kereta mini', 'ideaimg' => 'https://picsum.photos/288/163', 'ideadescription'=>'Inilah kereta yang bisa dipakai untuk menjelajahi dunia bawah tanah.', 'ideatarget'=>'77000000', 'currentearning'=>'10000000', 'donatorcount'=>'777','tierAmount'=>'1', 'ideadeadline'=>'2020-12-25', 'qa'=>'Apakah bisa dipakai untuk pergi keluar kota? Tentu saja bisa hehe.'],
-        ]);
+
+        for($i=0;$i<3;$i++){
+            for($j=0;$j<7;j++){
+                for($k=0;$k<10;$k++){
+                    $data[$i] = [
+                        'userid' => $i+1, 
+                        'categoryid' => $j+1, 
+                        'ideaname' => 'Ini Idea ke '.($i+1), 
+                        'ideaimg' => 'https://picsum.photos/288/163', 'ideadescription'=>'Lorem ipsum dolor, sit amet consectetur adipisicing elit!', 
+                        'ideatarget'=>'1450000', 
+                        'currentearning'=>'1000000', 
+                        'donatorcount'=>200,
+                        'tierAmount'=>3, 
+                        'ideadeadline'=>'2020-12-12', 
+                        'qa'=>'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae !',
+                        ];
+                }
+            }
+        }
+        DB::table('Idea')->insert($data);
     }
 }
